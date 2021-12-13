d1af1c796473d6b7d299186ea3a6a413f036b51f
takahiro.uoki <takaihro.uoki.hiryu0604@gmail.com>
2021-12-13 14:32:01 +0900

    CreateServlet.javaを修正

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -67,21 +67,13 @@ public class CreateServlet extends HttpServlet {
                 // データベースに保存
                 em.persist(m);
                 em.getTransaction().commit();
-                request.getSession().setAttribute("flish", "登録が完了しました。");
+                request.getSession().setAttribute("flush", "登録が完了しました。");
                 em.close();
 
                 // indexのページにリダイレクト
                 response.sendRedirect(request.getContextPath() + "/index");
             }
-
-            // データベースに保存
-            em.persist(m);
-            em.getTransaction().commit();
-            request.getSession().setAttribute("flush", "登録が完了しました。");
-            em.close();
-
-            response.sendRedirect(request.getContextPath() + "/index");
-        }
+      }
     }
 
 }
